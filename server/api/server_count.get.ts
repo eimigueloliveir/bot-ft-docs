6b2ff3e913d692bcfceacd4212cf4acf8d005c6c
Miguel Oliveira <migueloliveiradasneves@gmail.com>
2023-10-13 18:33:18 -0300

    created api to get server count

---
+++ server/api/server_count.get.ts
@@ -0,0 +1,17 @@
+
+export default defineEventHandler(async (event) => {
+
+    //event.respondWith(new Response(JSON.stringify({...process.env})));
+    let bot: Bot = await $fetch('https://discord.com/api/applications/@me',
+    {
+        headers: {
+            'Authorization': `Bot ${process.env.DISCORD_BOT_TOKEN}`
+        }
+    });
+
+    event.respondWith(new Response(JSON.stringify(bot.approximate_guild_count)));
+});
+
+type Bot = {
+    approximate_guild_count: number
+}
